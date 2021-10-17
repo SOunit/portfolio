ce43603c210b044879dd6d532bdf00d4bb64d192
SOunit <workbookwork7@gmail.com>
2021-10-17 12:53:29 -0700

    change welcome section style

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,10 +1,12 @@
-import SectionTitle from '../sectionTitle/SectionTitle';
 import classes from './Welcome.module.css';
 
 const Welcome = () => {
   return (
     <div className={classes['welcome']}>
-      <SectionTitle title='Thank you for visiting my portfolio page!' />
+      <h2 className={classes['welcome__title']}>Thank you</h2>
+      <p className={classes['welcome__caption']}>
+        for visiting my portfolio page!
+      </p>
       <p className={classes['welcome__text']}>
         I have 4.5 years experience in Web Development mainly as a Java
         Engineer!
