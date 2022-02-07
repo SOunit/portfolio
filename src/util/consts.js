3503563dbb3413141fd56265a82719a86506aa01
SOunit <workbookwork7@gmail.com>
2022-02-07 00:29:10 -0800

    fix data

--- src/util/consts.js
+++ src/util/consts.js
@@ -1,71 +1,83 @@
 // place
-export const OSAKA_JAPAN = 'Osaka, Japan';
+export const OSAKA_JAPAN = "Osaka, Japan";
 
 // work flow
-export const BASIC_DESIGN = 'Basic design';
-export const DETAIL_DESIGN = 'Detail design';
-export const CODING = 'Coding';
-export const UNIT_TEST = 'Unit test';
-export const INTEGRATION_TEST = 'Integration test';
-export const SYSTEM_TEST = 'System test';
+export const BASIC_DESIGN = "Basic design";
+export const DETAIL_DESIGN = "Detail design";
+export const CODING = "Coding";
+export const UNIT_TEST = "Unit test";
+export const INTEGRATION_TEST = "Integration test";
+export const SYSTEM_TEST = "System test";
 
 // position
-export const SOFTWARE_ENGINEER = 'Software Engineer';
-export const HTML_CODER = 'HTML coder';
+export const JAVA_FULL_STACK_DEVELPER = "Java Full Stack Developer";
+export const REACT_FULL_STACK_DEVELPER = "React Full Stack Developer";
+export const HTML_CODER = "HTML coder";
 
 // front
-export const HTML5 = 'HTML5';
-export const CSS3 = 'CSS3';
-export const JAVA_SCRIPT = 'JavaScript';
-export const TYPE_SCRIPT = 'TypeScript';
-export const J_QUERY = 'jQuery';
+export const HTML5 = "HTML5";
+export const CSS3 = "CSS3";
+export const JAVA_SCRIPT = "JavaScript";
+export const TYPE_SCRIPT = "TypeScript";
+export const J_QUERY = "jQuery";
 
 // front framework
-export const REACT = 'React.js';
+export const REACT = "React.js";
+export const REACT_NATIVE = "React Native";
+
+// front design
+export const ATOMIC_DESIGN = "Atomic design";
 
 // back
-export const JAVA = 'Java';
-export const PHP = 'PHP';
-export const VBA = 'VBA';
+export const GOLANG = "Golang";
+export const JAVA = "Java";
+export const PHP = "PHP";
+export const VBA = "VBA";
 
 // back framework
-export const NODE_JS = 'Node.js';
-export const EXPRESS = 'Express';
-export const GRAPHQL = 'Graphql';
-export const SOCKET_IO = 'Socket.IO';
+export const NODE_JS = "Node.js";
+export const EXPRESS = "Express";
+export const GRAPHQL = "Graphql";
+export const SOCKET_IO = "Socket.IO";
+export const ECHO = "Echo(Golang)";
+
+// fullstack design
+export const DOMAIN_DRIVEN_DESIGN = "Domain Driven Design";
 
 // pkg
-export const WORDPRESS = 'Wordpress';
+export const WORDPRESS = "Wordpress";
 
 // ORM
-export const SEQUELIZE = 'Sequelize';
+export const SEQUELIZE = "Sequelize";
+export const MONGOOSE = "Mongoose";
 
 // db
-export const MONGO_DB = 'mongo DB';
-export const SQL_SERVER = 'SQL Server';
-export const POSTGRE_SQL = 'PostgreSQL';
-export const ORACLE_DB = 'Oracle DB';
-export const MY_SQL = 'MySQL';
+export const MONGO_DB = "mongo DB";
+export const SQL_SERVER = "SQL Server";
+export const POSTGRE_SQL = "PostgreSQL";
+export const ORACLE_DB = "Oracle DB";
+export const MY_SQL = "MySQL";
 
-// src control
-export const GIT = 'Git';
-export const GITHUB = 'GitHub';
-export const BITBUCKET = 'Bitbucket';
+// infra
+export const DOCKER = "Docker";
+export const EXPO = "Expo";
+export const GIT = "Git";
+export const GITHUB = "GitHub";
+export const BITBUCKET = "Bitbucket";
 
 // server
-export const DOCKER = 'Docker';
-export const NGINX = 'NGINX';
-export const TOMCAT = 'Tomcat';
-export const AWS = 'AWS';
-export const FIREBASE = 'Firebase';
-export const HEROKU = 'Heroku';
-export const LINUX = 'Linux';
+export const NGINX = "NGINX";
+export const TOMCAT = "Tomcat";
+export const AWS = "AWS";
+export const FIREBASE = "Firebase";
+export const HEROKU = "Heroku";
+export const LINUX = "Linux";
 
 // offset
-export const OFFSET_WORK_LOG = '75';
-export const OFFSET_TOOL_SET = '90';
-export const OFFSET_SIDE_PROJECTS = '70';
+export const OFFSET_WORK_LOG = "75";
+export const OFFSET_TOOL_SET = "90";
+export const OFFSET_SIDE_PROJECTS = "70";
 
 // links
-export const GITHUB_URL = 'https://github.com/SOunit/portfolio';
-export const LINKEDIN_URL = 'https://www.linkedin.com/in/sho-okawa-1249a6222/';
+export const GITHUB_URL = "https://github.com/SOunit/portfolio";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/sho-okawa-1249a6222/";
